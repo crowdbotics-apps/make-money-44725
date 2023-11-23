ed7a9400bf2b1102b483dc147524975bb1ca54ed
Crowdbotics <team@crowdbotics.com>
2023-11-23 07:15:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "make-money-44725",
+  url: "https://make-money-44725.botics.co",
+  api: "https://make-money-44725.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
